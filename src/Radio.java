2e03430f45ece78a49113343a9fd6d593d6f8b28
IvetteCardona22 <IvetteCardona22@localhost>
2017-01-16 10:05:50 -0600

    Datos

--- src/Radio.java
+++ src/Radio.java
@@ -1,4 +1,16 @@
+/*
+ * Radio 
+ * Algoritmos y Estructura de Datos - Hoja de Trabajo 1
+ * Integrantes: 
+ * Ivette Maria Cardona Cabrera 16020
+ * Yasmin Yaneth Chavez Montuzo 16101
+ * Andrea Maria Cordon Mayen 16076
+ * 
+ * Objetivo: poner en practica el polimorfismo.
+ * Descripción: clase abstracta de radio
+ */
 
 public class Radio {
+	
 
 }
