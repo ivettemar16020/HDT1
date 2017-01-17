5f79deae44edc7bbbf6bd45aaae8f8c0a5f638c2
1Yasmin <ychavez2014ig@gmail.com>
2017-01-17 17:32:01 -0600

    inicio de la clase controlRadio

--- src/ControlRadio.java
+++ src/ControlRadio.java
@@ -1,6 +1,47 @@
 
-public class ControlRadio {
+public class ControlRadio implements Radio {
 
 	private boolean apagado;
-	private boolean am_fm;
+	private int am;
+	private float fm;
+	private String emisora;
+	
+	/**
+	 * indica el estado del radio
+	 */
+	public void estado(){
+		
+	}
+	
+	/**
+	 * cambia de la estacion am a fm
+	 */
+	public void frecuencia(){
+		
+	}
+	
+	/**
+	 * cambial el dial de la estacion
+	 * @param:  indica el am y fm 
+	 */
+	public void cambiar(boolean cambio){
+		
+	}
+	
+	/**
+	 * permite guardar una emisora en uno de los botones
+	 * @param: la estacion que se desea guardar
+	 */
+	public void guardar(int emisora){
+		
+	}
+	
+	/**
+	 * permite seleccionar la emisora del boton
+	 * @param: la estacion que se selecciona
+	 */
+	public float seleccionar(int estacion){
+		
+	}
+	
 }
