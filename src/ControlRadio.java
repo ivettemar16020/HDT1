731cf2e2bb6cb386c0f13fb209bf4ad5079d8b44
andreamcm <cor16076@uvg.edu.gt>
2017-01-18 10:32:58 -0600

    Cambiar

--- src/ControlRadio.java
+++ src/ControlRadio.java
@@ -6,6 +6,30 @@ public class ControlRadio implements Radio {
 	private float fm;
 	private String emisora;
 	
+	// Constructor
+	public ControlRadio(){
+	int am = 590;
+	double fm = 86.7;
+	String emisora = "";
+	} 
+	
+	// Setters y Getters
+	
+	public void setAm(int am){
+		this.am = am;
+	}
+	
+	public int getAm(){
+		return am;
+	}
+	
+	public void setfm(int fm){
+		this.fm = fm;
+	}
+	
+	public double getfm(){
+		return fm;
+	}
 	/**
 	 * indica el estado del radio
 	 */
@@ -30,7 +54,24 @@ public class ControlRadio implements Radio {
 	 * @param:  indica el am y fm 
 	 */
 	public void cambiar(boolean cambio){
-		
+		if (i == true){
+			if (am <= 1600 && am >= 590){
+				am = am + 10;
+				System.out.println(am);
+			}
+			else 
+				am = 590;
+				System.out.println(am);
+		}
+		else if (i == false){
+			if (fm <= 86.7 && fm >= 89.9){
+				fm = fm + 0.2;
+				System.out.println(fm);
+			}
+			else
+				fm = 89.9;
+				System.out.println(fm);
+		}
 	}
 	
 	/**
